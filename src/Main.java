e6bdd1f9ca464e17126283c7980addc443a625c9
Мария Гридина <9_chasov@mail.ru>
2022-07-05 20:30:53 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int value = 2000; //цена  билета
+        int x = 20; //цена мили
+
+        System.out.println("Количество начисленных миль = " + value / x);
+    }
+
+}
